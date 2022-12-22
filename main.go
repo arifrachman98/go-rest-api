eb192bf685cf2cb63009648f53f43e584ddc7fe5
Arif Rachman Hakim <arifrachman149@gmail.com>
2022-12-22 14:45:10 +0700

    Add function updateEvent

--- main.go
+++ main.go
@@ -40,6 +40,27 @@ func createEvent(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(newEvent)
 }
 
+func updateEvent(w http.ResponseWriter, r *http.Request) {
+	eventID := mux.Vars(r)["id"]
+	var updateEvent Event
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintln(w, "Please enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &updateEvent)
+
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			singleEvent.Title = updateEvent.Title
+			singleEvent.Desc = updateEvent.Desc
+			events = append(events[:i], singleEvent)
+			json.NewEncoder(w).Encode(singleEvent)
+		}
+	}
+}
+
 func getOneEvent(w http.ResponseWriter, r *http.Request) {
 	eventID := mux.Vars(r)["id"]
 
